a31beb178a6cd785a1c04a5f0f0ad7374546a60b
R. Tyler Croy <tyler@monkeypox.org>
2017-12-13 16:07:38 -0800

    Slightly less indentations

--- vars/defaultsFor.groovy
+++ vars/defaultsFor.groovy
@@ -5,32 +5,37 @@
 def call(String platform) {
     switch (platform) {
         case 'java':
-            pipeline {
-                agent none
-                stages {
-                    stage('Test') {
-                        failFast true
-                        parallel {
-                            stage('Linux') {
-                                agent { docker 'maven:3-alpine' }
-                                steps {
-                                    sh 'mvn test -B'
-                                }
-                                post {
-                                    always {
-                                        junit testResults: '**/surefire-reports/**/*.xml', allowEmptyResults: true
-                                        archiveArtifacts artifacts: '**/*.jar', fingerprint: true
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+            defaultsForJava()
+            return
             break
 
         default:
             fail "I do not know what the defaults for `${platform}` should be :("
             break
+    }
+}
+
+void defaultsForJava() {
+    pipeline {
+        agent none
+        stages {
+            stage('Test') {
+                failFast true
+                parallel {
+                    stage('Linux') {
+                        agent { docker 'maven:3-alpine' }
+                        steps {
+                            sh 'mvn test -B'
+                        }
+                        post {
+                            always {
+                                junit testResults: '**/surefire-reports/**/*.xml', allowEmptyResults: true
+                                archiveArtifacts artifacts: '**/*.jar', fingerprint: true
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
