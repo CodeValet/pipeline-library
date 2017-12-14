1e8863ee5cd2fbb51ed0582350a3fdbb3bad276f
R. Tyler Croy <tyler@monkeypox.org>
2017-12-13 16:57:32 -0800

    Well that refactoring idea didn't work out
    How does this even work.
    
        groovy.lang.MissingPropertyException: No such property: none for class: defaultsFor
    	at org.codehaus.groovy.runtime.ScriptBytecodeAdapter.unwrap(ScriptBytecodeAdapter.java:53)
    	at org.codehaus.groovy.runtime.ScriptBytecodeAdapter.getProperty(ScriptBytecodeAdapter.java:458)
    	at com.cloudbees.groovy.cps.sandbox.DefaultInvoker.getProperty(DefaultInvoker.java:34)
    	at com.cloudbees.groovy.cps.impl.PropertyAccessBlock.rawGet(PropertyAccessBlock.java:20)
    	at defaultsFor.defaultsForJava(/var/jenkins_home/jobs/joni/branches/defaults/builds/5/libs/pipeline-library/vars/defaultsFor.groovy:20)
    	at org.jenkinsci.plugins.pipeline.modeldefinition.ModelInterpreter.call(jar:file:/var/jenkins_home/plugins/pipeline-model-definition/WEB-INF/lib/pipeline-model-definition.jar!/org/jenkinsci/plugins/pipeline/modeldefinition/ModelInterpreter.groovy:54)

--- vars/defaultsFor.groovy
+++ vars/defaultsFor.groovy
@@ -5,53 +5,49 @@
 def call(String platform) {
     switch (platform) {
         case 'maven':
-            defaultsForMaven()
+            pipeline {
+                agent none
+                options {
+                    buildDiscarder(logRotator(numToKeepStr: '10'))
+                    timeout(time: 1, unit: 'HOURS')
+                }
+                stages {
+                    stage('Test') {
+                        failFast true
+                        parallel {
+                            stage('Linux') {
+                                agent { docker 'maven:3-alpine' }
+                                steps {
+                                    sh 'mvn test -B'
+                                }
+                                post {
+                                    always {
+                                        junit testResults: '**/surefire-reports/**/*.xml', allowEmptyResults: true
+                                        archiveArtifacts artifacts: '**/*.jar', fingerprint: true
+                                    }
+                                }
+                            }
+                            stage('FreeBSD 11') {
+                                agent { label 'freebsd' }
+                                steps {
+                                    sh 'mvn test -B'
+                                }
+                                post {
+                                    always {
+                                        junit testResults: '**/surefire-reports/**/*.xml', allowEmptyResults: true
+                                        archiveArtifacts artifacts: '**/*.jar', fingerprint: true
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
             return
             break
 
         default:
             fail "I do not know what the defaults for `${platform}` should be :("
             break
-    }
-}
-
-void defaultsForMaven() {
-    pipeline {
-        agent none
-        options {
-            buildDiscarder(logRotator(numToKeepStr: '10'))
-            timeout(time: 1, unit: 'HOURS')
-        }
-        stages {
-            stage('Test') {
-                failFast true
-                parallel {
-                    stage('Linux') {
-                        agent { docker 'maven:3-alpine' }
-                        steps {
-                            sh 'mvn test -B'
-                        }
-                        post {
-                            always {
-                                junit testResults: '**/surefire-reports/**/*.xml', allowEmptyResults: true
-                                archiveArtifacts artifacts: '**/*.jar', fingerprint: true
-                            }
-                        }
-                    }
-                    stage('FreeBSD 11') {
-                        agent { label 'freebsd' }
-                        steps {
-                            sh 'mvn test -B'
-                        }
-                        post {
-                            always {
-                                junit testResults: '**/surefire-reports/**/*.xml', allowEmptyResults: true
-                                archiveArtifacts artifacts: '**/*.jar', fingerprint: true
-                            }
-                        }
-                    }
-                }
-            }
-        }
     }
 }
